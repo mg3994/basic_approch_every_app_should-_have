87684324093a33c33faeb93c21549c66c8c123ac
mg3994 <manishgautammg7@yahoo.com>
2024-08-21 22:39:46 +0530

    idea droped for connectivity plus

--- shared/component/lib/src/template/template.dart
+++ shared/component/lib/src/template/template.dart
@@ -1,31 +1,31 @@
-// bnb
+// // bnb
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class BNB extends StatefulWidget {
-  const BNB({super.key});
+// class BNB extends StatefulWidget {
+//   const BNB({super.key});
 
-  @override
-  State<BNB> createState() => _BNBState();
-}
+//   @override
+//   State<BNB> createState() => _BNBState();
+// }
 
-class _BNBState extends State<BNB> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        decoration: BoxDecoration(boxShadow: [
-          BoxShadow(
-              color: Colors.black.withOpacity(0.5),
-              blurRadius: 30,
-              offset: Offset(0, 20))
-        ]),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(30),
-          child: BottomNavigationBar(items: []),
-        ),
-      ),
-    );
-  }
-}
+// class _BNBState extends State<BNB> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       bottomNavigationBar: Container(
+//         margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+//         decoration: BoxDecoration(boxShadow: [
+//           BoxShadow(
+//               color: Colors.black.withOpacity(0.5),
+//               blurRadius: 30,
+//               offset: Offset(0, 20))
+//         ]),
+//         child: ClipRRect(
+//           borderRadius: BorderRadius.circular(30),
+//           child: BottomNavigationBar(items: []),
+//         ),
+//       ),
+//     );
+//   }
+// }
